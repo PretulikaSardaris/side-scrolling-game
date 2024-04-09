6f904ced98ab72d9f613ac8aeeac7c73d2deb5ba
PREETI SARDAR <82765024+PretulikaSardaris@users.noreply.github.com>
2024-04-09 05:43:26 +0200

    d

---
+++ script.js
@@ -0,0 +1,40 @@
+window.addEventListener('load' , function(){
+    const canvas = document.getElementById('canvas')
+    const ctx = canvas.getContext('2d')
+    canvas.width = 800;
+    canvas.height = 720;
+    const p = document.getElementById('p')
+
+    class InputHandler{
+constructor(){
+    this.key = [];
+    window.addEventListener('keydown' , function(e){
+        console.log(e.key)
+       
+       
+    })
+}
+    }
+    class Player{
+
+    }
+    class Background{
+
+    }
+    class Enemy{
+
+    }
+
+    function handleEnemies(){
+
+    }
+    
+    function displayStatus(){
+
+    }
+
+    const input = new InputHandler()
+    function animate(){
+
+    }
+})
